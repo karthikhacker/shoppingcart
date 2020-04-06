66c416f7bec5b36430cbfd648d9d61ce8d042d5e
System Administrator <root@sundararajans-MacBook-Air.local>
2020-04-06 12:44:33 +0530

    Added update and remove category api

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require('../models/category');
+const _ = require('lodash');
 const getErrorMessage = require('../helpers/errorHandler');
 //create
 exports.create = (req,res) => {
@@ -19,4 +20,39 @@ exports.categories = (req,res) => {
      }
      res.status(200).json(categories);
    })
+}
+// cateory by id
+exports.categoryById = (req,res,next,id) => {
+  Category.findById(id).exec((err,category) => {
+     if(err || !category){
+       return res.status(400).json({ error : ' category not found '})
+     }
+     req.category  = category;
+     next();
+  })
+}
+// Read
+exports.read = (req,res) => {
+   return res.json(req.category)
+}
+// update
+exports.update = (req,res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err,category) => {
+     if(err){
+       return res.status(400).json({ error : getErrorMessage(err)});
+     }
+     res.status(200).json(category);
+  })
+}
+// remove
+exports.remove = (req,res) => {
+  const category  = req.category;
+  category.remove((err) => {
+     if(err){
+       return res.status(400).json({ error : getErrorMessage(err) });
+     }
+     res.status(200).json({ message : 'Category removed.'})
+  })
 }
--- server/routes/category.js
+++ server/routes/category.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const  {create,categories } = require('../controllers/category');
+const  {create,categories, categoryById, read, update, remove } = require('../controllers/category');
 const  isAdmin  = require('../middlewares/isAdmin');
 
 router.post('/create/category', passport.authenticate('jwt',{session : false }), isAdmin, create);
 router.get('/categories',categories);
+router.get('/category/:categoryId',read);
+router.put('/category/:categoryId',passport.authenticate('jwt',{session : false}), isAdmin, update);
+router.delete('/category/:categoryId',passport.authenticate('jwt',{session : false}), isAdmin, remove);
+
+router.param('categoryId',categoryById)
 module.exports = router;
