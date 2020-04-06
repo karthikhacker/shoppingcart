71506b0a028cb1a158f7d98b3334216ccfd3861a
System Administrator <root@sundararajans-MacBook-Air.local>
2020-04-06 14:40:36 +0530

    Added product search, product by sold and sell api

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -87,3 +87,63 @@ exports.update = (req,res) => {
      res.status(200).json(product);
    })
 }
+// get products based on ell and arrival
+exports.list = (req,res) => {
+  let order = req.query.order ?  req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+  Product.find()
+   .populate('category')
+   .sort([[ sortBy, order ]])
+   .limit(limit)
+   .exec((err,product) => {
+     if(err){
+       return res.status(400).json({ error : getErrorMessage(err) })
+     }
+     res.status(200).json(product);
+   })
+}
+// list related
+exports.listRelated = (req,res) => {
+  const limit = req.query.limit ? parseInt(req.query.limit) : 6;
+  Product.find({ _id : {$ne : req.product}, categoey : req.product.category })
+   .limit(limit)
+   .populate('category','_id, name')
+   .exec((err,product) => {
+      if(err){
+        return res.status(400).json({ error : getErrorMessage(err)})
+      }
+      res.status(200).json(product);
+   })
+}
+//List by search
+exports.listBySearch = (req,res) => {
+  let order = req.body.filter ? req.body.filter : "desc";
+  let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = parseInt(req.body.skip);
+  let findArgs = {};
+  for(let key in req.body.filters){
+     if(req.body.filters[key].length > 0){
+       if(key === 'price'){
+         findArgs[key] = {
+           $gte : req.body.filters[key][0],
+           $lte : req.body.filters[key][1]
+         }
+       }else{
+         findArgs[key] = req.body.filters[key];
+       }
+     }
+  }
+  Product.find(findArgs)
+   .populate('category')
+   .sort([[ sortBy, order ]])
+   .limit(limit)
+   .skip(skip)
+   .exec((err,product) => {
+     if(err){
+       return res.status(400).json({ error : getErrorMessage(err) })
+     }
+     res.status(200).json({ size : product.length, product});
+   })
+}
--- server/routes/product.js
+++ server/routes/product.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const  { productImageUpload, createProduct, productById, read, remove, update } = require('../controllers/product');
+const  { productImageUpload, createProduct, productById, read, remove, update, list, listRelated, listBySearch } = require('../controllers/product');
 const  isAdmin  = require('../middlewares/isAdmin');
 
 router.post('/product/image', passport.authenticate('jwt',{session : false }), isAdmin, productImageUpload);
 router.post('/create/product',passport.authenticate('jwt',{session : false}), isAdmin, createProduct);
 router.get('/product/:productId',read);
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+router.post('/products/by/search', listBySearch);
 router.delete('/product/:productId',passport.authenticate('jwt',{session : false }), isAdmin, remove);
 router.put('/product/:productId',passport.authenticate('jwt',{session : false }), isAdmin, update);
 
