da756d587be090781ac17a2bf405848fce8b7b51
your_username <you@example.com>
2020-07-01 10:06:25 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -43,14 +43,14 @@ app.use('/uploads',express.static('uploads'));
 
 //serve static files in production
 if(process.env.NODE_ENV === 'production'){
-   express.static(express.static('frontend/bulid'))
+   express.static(express.static('frontend/build'))
    app.get("*",(req,res) => {
       res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
    })
 }
 
 //server
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
    console.log(chalk.white(`App running at port  ${port}` ));
 })
