30a50fd603166ac2a3589c5a72568796d7432dc8
your_username <you@example.com>
2020-07-01 14:53:29 +0530

    Modified

--- server/controllers/braintree.js
+++ server/controllers/braintree.js
@@ -1,11 +1,11 @@
 const braintree= require('braintree');
-require('dotenv').config();
+const config = require('../config/db');
 
 const gateway = braintree.connect({
   environment : braintree.Environment.Sandbox,
-  merchantId : process.env.BRAINTREE_MERCHANT_ID,
-  publicKey : process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey : process.env.BRAINTREE_PRIVATE_KEY
+  merchantId : config.BRAINTREE_MERCHANT_ID,
+  publicKey : config.BRAINTREE_PUBLIC_KEY,
+  privateKey : config.BRAINTREE_PRIVATE_KEY
 })
 //stripe payment
 exports.getToken = (req,res) => {
